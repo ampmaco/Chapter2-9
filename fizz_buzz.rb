2f438be892306cfd16d78c9d3f2ed234b9abb335
林晃弘 <hayashiakihiro@hayashiakihironoMacBook-puro.local>
2019-04-07 15:46:36 +0900

    [Fix] 2-9

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,12 +1,16 @@
 def fizz_buzz(number)
-	if number%3 == 0
+
+	if number%15 == 0
+		puts "FizzBuzz"
+
+	elsif number%3 == 0
 		puts "Fizz"
 	elsif number%5 == 0
 		puts "Buzz"
-	elsif number%15 == 0
-		puts "FizzBuzz"
+
 	else
 		puts number.to_s
+		
 	end
 end
 
